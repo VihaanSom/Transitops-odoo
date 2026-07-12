42093f7cde2657d44adeaff8fcd6b0920f5ff8ea
Vihaan Sompura <131134379+VihaanSom@users.noreply.github.com>
2026-07-12 10:59:34 +0530

    initialized prisma with database and env package added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
 const express = require('express');
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require('@prisma/client');
 
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
 const app = express();
-const prisma = new PrismaClient({});
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
